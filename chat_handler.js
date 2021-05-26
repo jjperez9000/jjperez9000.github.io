a12f0f695c613b1a1bf38ce10f5dd7c1f6f40964
jjperez9000 <jjperez@clemson.edu>
2021-05-26 12:23:07 -0400

    Update chat_handler.js

--- chat_handler.js
+++ chat_handler.js
@@ -58,25 +58,25 @@ function checkPresence() {
 	clearInterval(presenceIntervalCheck);
 }
 
-// document.querySelector("a-scene").addEventListener("chatevent", e => {
+document.querySelector("a-scene").addEventListener("chatevent", e => {
 
-// 	var mySplit = e.detail.text.split(":");
-// 	console.log("e.detail.text = " + mySplit[1]);
+	var mySplit = e.detail.text.split(":");
+	console.log("e.detail.text = " + mySplit[1]);
 	
 	
-// 	// function we want to run we add mod_ to the string to isolate our custom functions
-// 	// from the global namespace and prevent people from running functions through chat
-// 	// interface unless it's one we've added for that purpose.
+	// function we want to run we add mod_ to the string to isolate our custom functions
+	// from the global namespace and prevent people from running functions through chat
+	// interface unless it's one we've added for that purpose.
 	
-// 	var fnstring = "mod_" + mySplit[1];
+	var fnstring = "mod_" + mySplit[1];
 
-// 	// find object
-// 	var fn = window[fnstring];
+	// find object
+	var fn = window[fnstring];
 
-// 	// is object a function?
-// 	if (typeof fn === "function"){
-// 		fn();
-// 	}else{
-// 		console.log(fn + " is not a function");
-// 	}
-// });
+	// is object a function?
+	if (typeof fn === "function"){
+		fn();
+	}else{
+		console.log(fn + " is not a function");
+	}
+});
