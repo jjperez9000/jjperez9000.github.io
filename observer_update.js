82975655ce0d897fced389551ccac89170e903a0
jjperez9000 <jjperez@clemson.edu>
2021-05-27 10:37:02 -0400

    Update observer_update.js

--- observer_update.js
+++ observer_update.js
@@ -6,13 +6,14 @@ var chatButton = document.querySelectorAll("[class*=accent4]");
 console.log(chatButton);
 
 var buttonIntervalCheck;
-var buttonFound = false;
+var currentButton;
 
 if (document.querySelector("[class*=accent4]") == null) {
     console.log("chat button does not exist yet");
     buttonIntervalCheck = setInterval(function() {
         if (document.querySelector("[class*=accent4]") != null) {
             console.log("found the button!!!!!!!!!!!!");
+            chatButton = document.querySelector("[class*=accent4]"); 
             clearInterval(buttonIntervalCheck);
         } else {
             console.log("looking for chat button");
@@ -21,4 +22,4 @@ if (document.querySelector("[class*=accent4]") == null) {
 } else {
     console.log("found the button (on first try)");
 }
-
+chatButton.onclick = function() {console.log("button does thing");}
