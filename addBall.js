bf0f200c9c4258cd55b18ba4c6861a27540c3090
jjperez9000 <jjperez@clemson.edu>
2021-06-02 11:04:47 -0400

    Update addBall.js

--- addBall.js
+++ addBall.js
@@ -1,93 +1,113 @@
-function inject_createInteractiveBall() {
-				
-	//Query assets in order to setup template
-	let assets = document.querySelector("a-assets");
-	// create a new template variable
-	let newTemplate = document.createElement("template");
-	// create template id
-	newTemplate.id = "interactable-ball-media";
-	// create a new entity for the template so we can append it to the assets later
-	// normally this is done in the Hubs.html "bootstrap" file
-	let newEntity = document.createElement("a-entity");
-				
-	// setup the attributes for the template such and class and components that
-	// should be associated with the template entities
-				
-	// set the class to interactable if you want interaction or some other class
-	// based on hubs interaction layers
-	newEntity.setAttribute("class", "interactable");
-				
-	// for attributes with multiple objects in the schema it's easier to setup
-	// a varibable to hold the attribute and its values then create the node on
-	// the entity
-				
-	// the body helper component allows you to setup dynamic attributes for physics
-	// interactions.  the type can be dynamic or static.  collision filters and
-	// masks are used to limit what objects can collide with.  See the body-helper
-	// component for more information
-	let bh = document.createAttribute("body-helper");
-	bh.value = "type: dynamic; mass: 1; collisionFilterGroup: 1; collisionFilterMask: 15;";
-	newEntity.setAttributeNode(bh);
-				
-	// An object needs to have geometry in order to be visible and work with physics
-	// here we reuse the bh variable since the body helper node has been added to the entity.  In this case we are creating the geometry attribute (see aframe docs)
-	bh = document.createAttribute("geometry");
-	// create a sphere geometry with a radius of 0.5 meters
-	bh.value = "primitive: sphere; radius: 0.5";
-	newEntity.setAttributeNode(bh);
-				
-	// reuse the same bh variable for a material attribute to color the geometry
-	bh = document.createAttribute("material");
-	// set the color to yellow.  You can set a lot of things here, texture, shininess etc.  See the aframe docs on materials
-	bh.value = "color:yellow;metalness:1.0;roughness:0.0;";
-	newEntity.setAttributeNode(bh);
-				
-	// set the unowned body kinematic component for the object since it's networked
-	// and physics related.
-	newEntity.setAttribute("set-unowned-body-kinematic", "");
-	// sets the remote hover target component on the object
-	newEntity.setAttribute("is-remote-hover-target", "");
-				
-	// the tags component allows you to filter the collisions and interactable
-	// qualities of the entity.  We can reuse bh to set all it's values
-	bh = document.createAttribute("tags")
-	// set it to be a hand collision target, holdable, give it a hand constraint, a remote constraint, and set to be inspectable with a right click.
-	bh.value = "isHandCollisionTarget: true; isHoldable: true; offersHandConstraint: true; offersRemoteConstraint: true; inspectable: true;";
-	newEntity.setAttributeNode(bh);
-				
-	// you can set the objects to be destroyed at extreme distances in order to avoid having a bunch of hard to find physics objects falling in your hub
-	newEntity.setAttribute("destroy-at-extreme-distances", "");
-	// sets whether the object can be scaled when you grab it. Check hubs docs or the component to see how it can be scaled in different modes
-	newEntity.setAttribute("scalable-when-grabbed", "");
-				// another component setup.  Check it out in the components in src
-	newEntity.setAttribute("set-xyz-order", "");
-	// important! since the matrix auto update on objects in turned off by default
-	// in order to save compute power
-	newEntity.setAttribute("matrix-auto-update", "");
-	// whether this object has a hoverable visuals interaction. You may have to add additional child entities to the template to get this to show up.  Check the component to see how it works 
-	newEntity.setAttribute("hoverable-visuals", "");
-
-	// Important!  This Component helps you set the collision shape for the object
-	// without it set on the actual entity which contains the mesh (set with the 
-	// geometry component above in this case) the physics won't collide and the 
-	// object will fall through the ground.  Check the component for details
-	bh = document.createAttribute("shape-helper")
-	bh.value = "";
-	newEntity.setAttributeNode(bh);
-				
-	//add the listed-media component
-	newEntity.setAttribute("listed-media", "");
+function inject_arcade_asteroids() {
+		
+	var myBody = document.querySelector("body");
+	var myAssets = document.querySelector("a-assets");
 	
-	//add the camera-cube-env component
-	newEntity.setAttribute("camera-cube-env", "");
-				
-	//Once all the attributes are setup on the entity you can append it to the template variable content created above.
-	newTemplate.content.appendChild(newEntity);
-				
-	// once the template is created you append it to the assets
-	assets.appendChild(newTemplate);
-				
-				
+	var gameSrcUrl = "https://colinfizgig.github.io/Custom-Hubs-Components/components/asteroids/agame.js";
+	var gameKeyMapUrl = "https://colinfizgig.github.io/Custom-Hubs-Components/components/asteroids/AGameKeymapping.js";
+	
+	var newLink = document.createElement("link");
+	newLink.rel = "stylesheet";
+	newLink.href = "https://colinfizgig.github.io/Custom-Hubs-Components/components/asteroids/style.css";
+	document.querySelector("head").appendChild(newLink);
+	
+	var newScript = document.createElement("script");
+	newScript.type = 'text/javascript';
+	srcAt = document.createAttribute('src');
+	srcAt.value = gameSrcUrl;
+	newScript.setAttributeNode(srcAt);
+	myBody.appendChild(newScript);
+	
+	newScript = document.createElement("script");
+	newScript.type = 'text/javascript';
+	srcAt = document.createAttribute('src');
+	srcAt.value = gameKeyMapUrl;
+	newScript.setAttributeNode(srcAt);
+	myBody.appendChild(newScript);
+
+	var cabinetUrl = "https://colinfizgig.github.io/aframe_Components/asteroids/AsteroidsCabinetMerged.glb";
+	//var screenUrl = "https://colinfizgig.github.io/aframe_Components/asteroids/Asteroids/Screen1.obj";
+	
+	var myText = document.createElement("input");
+	myText.type = "text";
+	myText.id = "namefield";
+	document.body.insertBefore(myText, document.body.firstChild);
+	
+	// preload the cabinet asset in assets
+	var cabinetAsset = document.createElement("a-asset-item");
+	cabinetAsset.id = "cabinet";
+	//cabinetAsset.response-type = "arraybuffer";
+	
+	var srcAt = document.createAttribute('src');
+	srcAt.value = cabinetUrl;
+	cabinetAsset.setAttributeNode(srcAt);
+
+	myAssets.appendChild(cabinetAsset);
+	
+	// create a template for the cabinet
+	
+	var myTemplate = document.createElement("template");
+	myTemplate.id = "asteroids-game";
+	
+	// parent entity so screen goes with cabinet
+	var myAsteroidsCabinet = document.createElement("a-entity");
+	myAsteroidsCabinet.id = "AsteroidsCabinet";
+	myAsteroidsCabinet.position = "0 0 0";
+	
+	var cabModel = document.createElement("a-entity");
+	var newAtt = document.createAttribute('gltf-model');
+	newAtt.value = "#cabinet";
+	cabModel.setAttributeNode(newAtt);
+	
+	newAtt = document.createAttribute('camera-cube-env');
+	newAtt.value = "resolution:256; interval: 1000; distance: 1000; matoverride: true; metalness: 0.0; roughness: 0.0; repeat: false";
+	cabModel.setAttributeNode(newAtt);
+
+	newAtt = document.createAttribute('shadow');
+	newAtt.value = "";
+	cabModel.setAttributeNode(newAtt);
+
+	newAtt = document.createAttribute('position');
+	newAtt.value = "0 -2.15 0";
+	cabModel.setAttributeNode(newAtt);
+	
+	newAtt = document.createAttribute('scale');
+	newAtt.value = "1 1 1";
+	cabModel.setAttributeNode(newAtt);
+	
+	myAsteroidsCabinet.appendChild(cabModel);
+	
+	var scrnModel = document.createElement("a-plane");
+	scrnModel.id = "gamescreen";
+	
+	/*
+	newAtt = document.createAttribute('src');
+	newAtt.value = "#asteroids";
+	scrnModel.setAttributeNode(newAtt);*/
+	
+	newAtt = document.createAttribute('position');
+	newAtt.value = "0 -.7 0";
+	scrnModel.setAttributeNode(newAtt);
+	
+	newAtt = document.createAttribute('scale');
+	newAtt.value = ".75 .75 .75";
+	scrnModel.setAttributeNode(newAtt);
+	
+	newAtt = document.createAttribute('material');
+	newAtt.value = "src:#asteroids; shader: flat";
+	scrnModel.setAttributeNode(newAtt);
+	
+	newAtt = document.createAttribute('canvas-updater');
+	newAtt.value = "";
+	scrnModel.setAttributeNode(newAtt);
+	
+	myAsteroidsCabinet.appendChild(scrnModel);
+	
+	myTemplate.content.appendChild(myAsteroidsCabinet);
+	myAssets.appendChild(myTemplate);
+	
+	
+	
 	//	This sets up an update function for how often each networked entity needs to update
 	// position, rotation, or scale based on each transforms setting in the NAF schema.
 	// I'm not sure why it's not a utility function in NAF?
@@ -104,15 +124,18 @@ function inject_createInteractiveBall() {
 					return true;
 				}
 
-				return false;
+			return false;
 			};
 		};
 	};
-
+						
+						
 	// Add the new schema to NAF. and declare the networked components and their update 
 	// sensitivity using the function above if they modify the transforms.
 	NAF.schemas.add({
-		template: "#interactable-ball-media",
+		// template to add (created above)
+		template: "#asteroids-game",
+
 		components: [
 			{
 				component: "position",
@@ -126,33 +149,34 @@ function inject_createInteractiveBall() {
 				component: "scale",
 				requiresNetworkUpdate: vectorRequiresUpdate(0.001)
 			},
+			// TODO: Optimize checking mediaOptions with requiresNetworkUpdate.
 			"media-loader",
-			"material",
 			"pinnable"
+		],
+		nonAuthorizedComponents: [
+			
 		]
 	});
-		
+
 }
 
-// we add the prefix inject_ to our utility functions to isolate them from the global namespace
-inject_createInteractiveBall();
+inject_arcade_asteroids();
 
-// we add the prefix mod_ to this function to allow it to be targeted by the chat interface
-function mod_addBall(){
+function inject_GameElements(){
+	mod_createGame();
 	
-	if(document.querySelector("a-entity[camera-cube-env]") == null){
-
-		var el = document.createElement("a-entity")
-		el.setAttribute("networked", { template: "#interactable-ball-media" } )
-		el.object3D.position.y = 2;
-		AFRAME.scenes[0].appendChild(el)
-		
-	}else{
-		console.log("a ball already exists");
-	}
-	
+	var el = document.createElement("a-entity")
+	el.setAttribute("id", "game")
+	el.setAttribute("networked", { template: "#asteroids-game" } )
+	el.setAttribute("media-loader", {animate: false, fileIsOwned: true})
+	el.object3D.position.y = 2;
+	AFRAME.scenes[0].appendChild(el)
 }
 
+function mod_createGame() {
+	var game = new Game();
+	game.run();
+}
 function mod_genius() {
     console.log("John is a genius");
 }
