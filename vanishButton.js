58298566d29d237ad950d721171dd43548302672
jjperez9000 <jjperez@clemson.edu>
2021-06-03 13:06:58 -0400

    Update vanishButton.js

--- vanishButton.js
+++ vanishButton.js
@@ -1,15 +1,17 @@
 
 var visible = true;
+var lastItemClicked;
 function mod_hideRoom() {
     if (visible) {
-        document.querySelector(".CombinedMesh").setAttribute("visible", false);
+        document.querySelector(lastItemClicked.id).setAttribute("visible", false);
         visible = false;
     } else {
-        document.querySelector(".CombinedMesh").setAttribute("visible", true)
+        document.querySelector(lastItemClicked.id).setAttribute("visible", true)
         visible = true;
     }
 }
 
 document.addEventListener("click", function(){
     console.log(AFRAME.scenes[0].systems.interaction.state.rightRemote.held);
+    lastItemClicked = AFRAME.scenes[0].systems.interaction.state.rightRemote.held;
 })
