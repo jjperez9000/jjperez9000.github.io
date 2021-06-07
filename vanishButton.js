e40c98fad30c8e1cfc7260d2eb5033b734402b4e
jjperez9000 <jjperez@clemson.edu>
2021-06-07 11:16:59 -0400

    Update vanishButton.js

--- vanishButton.js
+++ vanishButton.js
@@ -33,7 +33,7 @@ function inject_createInteractiveBall() {
 	// normally this is done in the Hubs.html "bootstrap" file
 	let newEntity = document.createElement("a-entity");
 
-	let newChild = document.createElement("a-entity");
+	// let newChild = document.createElement("a-entity");
 	newEntity.setAttribute("class", "ui interactable-ui");
 	newEntity.innerHTML = "<a-entity class='snap-button' mixin='rounded-action-button' is-remote-hover-target='' tags='singleActionButton: true;' position='0 0.15 0.08' scale='0.75 0.75 0.75' slice9='' text-button=''><a-entity sprite='' class='snap-icon' icon-button='image: snap_camera.png; hoverImage: snap_camera.png;' scale='0.2 0.2 0.2' position='0 0.002 0.001'></a-entity></a-entity>"
 				
