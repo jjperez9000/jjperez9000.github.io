f04397b1ce53843e5181aab511b748e33e0a2da4
jjperez9000 <jjperez@clemson.edu>
2021-06-07 10:04:34 -0400

    Update vanishButton.js

--- vanishButton.js
+++ vanishButton.js
@@ -179,5 +179,3 @@ function mod_addBall(){
 	}
 	
 }
-
-<a-entity class="snap-button" mixin="rounded-action-button" is-remote-hover-target="" tags="singleActionButton: true;" position="0 0.15 0.08" scale="0.75 0.75 0.75" slice9="" text-button=""><a-entity sprite="" class="snap-icon" icon-button="image: snap_camera.png; hoverImage: snap_camera.png;" scale="0.2 0.2 0.2" position="0 0.002 0.001"></a-entity></a-entity>
