42f5cfbbffc5ccdd5c475178b8d15e662de1ecb3
jjperez9000 <jjperez@clemson.edu>
2021-06-10 10:48:28 -0400

    Update slideshow-template.js

--- slideshow-template.js
+++ slideshow-template.js
@@ -34,7 +34,7 @@
 		// here we reuse the bh variable since the body helper node has been added to the entity.  In this case we are creating the geometry attribute (see aframe docs)
 		bh = document.createAttribute("geometry");
 		//create a simple geometry sphere of 0.2 meters
-		bh.value = "primitive: cube; radius: 0.2";
+		bh.value = "primitive: box; radius: 0.2";
 		newEntity.setAttributeNode(bh);
 
 		// set the unowned body kinematic component for the object since it's networked
